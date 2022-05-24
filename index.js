7d0aacbac9b15afb1faa35123c7693a48457679a
Tom <tomzhongcn@gmail.com>
2022-05-24 10:58:15 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+io.on("connection", socket => {
+  io.emit("chat message", "a new user has joined")
+  socket.on("chat message", msg => {
+    io.emit("chat message", msg)
+  })
+  socket.on("disconnect", () => {
+    io.emit("chat message", "a user has left the conversation")
+  })
+})
+
+server.listen(3000, () => {
+  console.log("listen on *:3000")
+})
